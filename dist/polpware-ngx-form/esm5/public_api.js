f5967b0aff0b3c4567b15b91487721169461b7e6
Xiaolong <xiaolong@cse.tamu.edu>
2019-01-18 09:33:48 -0500

    Remove old publication

--- dist/polpware-ngx-form/esm5/public_api.js
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview added by tsickle
- * @suppress {checkTypes,extraRequire,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
- */
-/*
- * Public API Surface of polpware-ngx-form
- */
-export { validateUrl, normalizeHost, normalizeUrl, buildUrlValidator } from './lib/validators/url-validator';
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicHVibGljX2FwaS5qcyIsInNvdXJjZVJvb3QiOiJuZzovL3BvbHB3YXJlLW5neC1mb3JtLyIsInNvdXJjZXMiOlsicHVibGljX2FwaS50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBSUEsNEVBQWMsZ0NBQWdDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyIvKlxuICogUHVibGljIEFQSSBTdXJmYWNlIG9mIHBvbHB3YXJlLW5neC1mb3JtXG4gKi9cblxuZXhwb3J0ICogZnJvbSAnLi9saWIvdmFsaWRhdG9ycy91cmwtdmFsaWRhdG9yJztcbiJdfQ==
